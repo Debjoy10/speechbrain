86bfe0336c68ecc9aaec96756b7e6937656260da
Debjoy10 <sahadebjoy10@gmail.com>
2022-12-19 09:50:14 +0530

    HpOpt-manual done

---
+++ recipes/IMSV/SpeakerRec/hparams/hpopt/combined_run.sh
@@ -0,0 +1,56 @@
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_00.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_01.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_02.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_03.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_04.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_05.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_06.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_07.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_08.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_09.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_10.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt/train_tf_11.yaml --debug --debug_batches 5000
+rm -rf ../../../../IMSV/results/tf_con_16k_HPOPT/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_00.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_01.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_02.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_03.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_04.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_05.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_06.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_07.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_08.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_09.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_10.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_11.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_12.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_13.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_14.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_15.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_test_ML/
---
+++ recipes/IMSV/SpeakerRec/hparams/hpopt_lang/run.sh
@@ -0,0 +1,32 @@
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_00.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_01.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_02.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_03.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_04.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_05.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_06.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_07.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_08.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_09.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_10.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_11.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_12.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_13.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_14.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
+CUDA_VISIBLE_DEVICES="1" python3 train_speaker_embeddings.py hparams/hpopt_lang/train_ecapa_tdnn_ML_pretrained_15.yaml
+rm -rf ../../../../IMSV/results/ecapa_augment_16k_pretrained_HPOPT_ML/
